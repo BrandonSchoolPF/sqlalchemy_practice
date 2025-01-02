8168f511f009031968d0abf51bc9bcf5307f2d6e
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2025-01-01 20:49:54 -0500

    added router to look at insert route

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.schemas import CompanyCreate
-from app.database import SessionLocal
-from app.models import Company
+from src.insert.insert_routes import insert_router
+
+
 app = FastAPI()
 
-# Dependency to get a database session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-@app.post("/companies/", response_model=CompanyCreate)
-def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
-    db_company = db.query(Company).filter(Company.name == company.name).first()
-    if db_company:
-        raise HTTPException(status_code=400, detail="Company already exists")
-
-    new_company = Company(name=company.name, address=company.address)
-    
-    db.add(new_company)
-    db.commit()
-    db.refresh(new_company)
-
-    return new_company
-
-
-@app.post("/addtest/", response_model=TestCreate)
-def add_test()
+app.include_router(insert_router, prefix='/insert')
