3cb7b4d92b27467339f5e73933bf175321c50855
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2025-01-04 22:18:00 -0500

    Trying out autodocstrings

--- src/select/select_route.py
+++ src/select/select_route.py
@@ -5,9 +5,11 @@ from app.models import Company
 
 get_data = APIRouter()
 
-@get_data.get("/company_name")
-def get_company_name(id, db: Session = Depends(get_db)):
-    company_name = db.query(Company).filter(Company.id == id).first()
+@get_data.get("/company_name/{company_id}")
+def get_company_name(company_id, db: Session = Depends(get_db)):
+    company_name = db.query(Company).filter(Company.id == company_id).first()
     print(company_name)
     if company_name:
         return {'Company ID': id, 'Company Name': company_name.name, 'Company Address':company_name.address, 'Date Created':company_name.created_at}
+    else:
+        return {f'Company {id} does not exist'}
